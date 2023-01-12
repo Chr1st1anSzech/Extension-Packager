aa647a0474a0652eb8e141452ee5dfa460746af0
Christian Szech <christian.szech@live.de>
2023-01-12 19:04:24 +0100

    Barrierefreiheit

--- Extension Packager Library/src/Viewmodels/MainPageViewModel.cs
+++ Extension Packager Library/src/Viewmodels/MainPageViewModel.cs
@@ -7,16 +7,23 @@ using Extension_Packager_Library.src.Helper;
 using Extension_Packager_Library.src.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Extension_Packager_Library.src.Viewmodels
 {
     public class MainPageViewModel : ViewModel
     {
         #region Public Properties
-        
+
         public List<string> BrowserFilterList { get; } = new() { "Alle", "Google Chrome", "Microsoft Edge" };
 
-        public List<DataModels.Extension> ExtensionsList { get; set; } = new();
+        private List<DataModels.Extension> _lastModifiedExtensions = new();
+        public List<DataModels.Extension> LastModifiedExtensions
+        {
+            get { return _lastModifiedExtensions; }
+            set { SetField(ref _lastModifiedExtensions, value); }
+        }
+
 
         private INavigationService _navigationService;
         public INavigationService NavigationService
@@ -27,6 +34,11 @@ namespace Extension_Packager_Library.src.Viewmodels
 
         #endregion
 
+        #region Private Fields
+
+        private List<DataModels.Extension> _notFilteredLastModifiedExtensions;
+
+        #endregion
 
         #region Commands
 
@@ -34,6 +46,7 @@ namespace Extension_Packager_Library.src.Viewmodels
         public MyCommand CreateExtensionCommand { get; set; }
         public MyCommand ShowExtensionsListCommand { get; set; }
         public MyCommand UpdateExtensionCommand { get; set; }
+        public MyCommand SearchCommand { get; set; }
 
         private void SetCommands()
         {
@@ -41,6 +54,7 @@ namespace Extension_Packager_Library.src.Viewmodels
             CreateExtensionCommand = new MyCommand(CreateExtension);
             ShowExtensionsListCommand = new MyCommand(ShowExtensionsList);
             UpdateExtensionCommand = new MyCommand(UpdateExtension);
+            SearchCommand = new MyCommand(Search);
         }
 
         #endregion
@@ -55,12 +69,31 @@ namespace Extension_Packager_Library.src.Viewmodels
         private void LoadExtensions()
         {
             IExtensionStorage extensionStorage = new DatabaseStorage();
-            ExtensionsList = extensionStorage.GetAllLastModified();
+            LastModifiedExtensions = extensionStorage.GetAllLastModified();
         }
 
         private void OpenSettings(object parameter = null)
         {
 
+        }
+
+        private void Search(object parameter = null)
+        {
+            if (parameter is string searchText && searchText != null)
+            {
+                _notFilteredLastModifiedExtensions ??= LastModifiedExtensions;
+
+                if (searchText.Equals(string.Empty))
+                {
+                    LastModifiedExtensions = _notFilteredLastModifiedExtensions;
+                    return;
+                }
+
+                LastModifiedExtensions = _notFilteredLastModifiedExtensions.Where(extension =>
+                {
+                    return extension.Name.ToLower().Contains(searchText.ToLower());
+                }).ToList();
+            }
         }
 
         private void UpdateExtension(object parameter = null)
@@ -72,7 +105,7 @@ namespace Extension_Packager_Library.src.Viewmodels
                 PageParameter param = new()
                 {
                     Extension = storage.Get(id),
-                    IsUpdate= true
+                    IsUpdate = true
                 };
                 _navigationService.Navigate("CrxSelectPage", param);
             }
--- Extension Packager/src/Controls/SearchField.xaml.cs
+++ Extension Packager/src/Controls/SearchField.xaml.cs
@@ -1,15 +1,44 @@
 // Copyright (c) Christian Szech
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using Extension_Packager_Library.src.Helper;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Automation;
 using Microsoft.UI.Xaml.Controls;
+using System.Windows.Input;
 
 namespace Extension_Packager.src.Controls
 {
     public sealed partial class SearchField : UserControl
     {
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+                                 DependencyProperty.Register("Command", typeof(ICommand),
+                                                             typeof(SearchField), new PropertyMetadata(null));
+
         public SearchField()
         {
             this.InitializeComponent();
+        }
+
+        private void SearchFieldTextBox_KeyUp(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if (e.Key != Windows.System.VirtualKey.Enter) return;
+
+            Command.Execute(SearchFieldTextBox.Text);
+        }
+
+        private void SearchFieldTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (SearchFieldTextBox.Text.Length == 0)
+            {
+                Command.Execute(string.Empty);
+            }
         }
     }
 }
